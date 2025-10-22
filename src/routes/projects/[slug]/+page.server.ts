7c9c511f3c6289095082ffe5561c014812337b6d
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-10-22 13:14:36 +0200

    Retry without adapter static

---
+++ src/routes/projects/[slug]/+page.server.ts
@@ -0,0 +1,18 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const data = await import('$lib/datasource.json');
+
+	const projects = (data as any).default?.projects ?? (data as any).projects ?? [];
+
+	const project = projects.find((p: any) => p.metadata.id === params.slug);
+
+	if (project) {
+		return {
+			project
+		};
+	}
+
+	error(404, 'Not found');
+};

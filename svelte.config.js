c3d83cbe460fb3c13223e2121cfbb99c79809212
TommasoPrinetti <Tommi.prinetti@gmail.com>
2025-10-22 13:48:37 +0200

    Updts

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
@@ -19,7 +16,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			assets: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		prerender: {
 			handleUnseenRoutes: 'warn'
